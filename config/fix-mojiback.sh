d35f8184da90f8e33abb62893645ee3891a3b4e6
Adrien Chinour <git@chinour.fr>
2021-01-29 01:57:24 +0100

    style: change file extension mojiback

--- config/fix-mojiback.sh
+++ config/fix-mojiback.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 for f in $(find $1 -name '*.json');
-  do cp "$f" "$f.backup"; cat "$f.backup" | jq . | iconv -f utf8 -t latin1 > "$f";
+  do cp "$f" "$f.backup"; cat "$f.backup" | jq . | iconv -f utf8 -t latin1 > "$f"; rm "$f.backup";
 done;
